f9ffc4a8f7735ed1681d59c176ad72c580be9945
marche1os <kolpmark@yandex.ru>
2023-06-24 10:55:52 +0300

    impl 42 lesson

---
+++ lesson_42/Program.fs
@@ -0,0 +1,11 @@
+﻿// 42.3
+let rec allSubsets n k =
+    if k = n then
+        [Set.ofSeq [1..n]]
+    else if k = 0 then
+             [Set.empty]
+    else
+        let left = allSubsets(n-1) k
+        let right = left |> List.map(fun x -> x.Add n)
+        left @ right
+   
