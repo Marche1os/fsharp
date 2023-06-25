cb961eeafb71da8b6510a03792e31154414c03e7
marche1os <kolpmark@yandex.ru>
2023-06-25 11:06:11 +0300

    fix bug

--- lesson_42/Program.fs
+++ lesson_42/Program.fs
@@ -1,11 +1,11 @@
 ﻿// 42.3
 let rec allSubsets n k =
-    if k = n then
-        set[set [1..n]]
-    else if k = 0 then
-             Set.empty
-    else
+    match (n, k) with
+    | (_, 0) -> set[Set.empty]
+    | (n, k) when n < k -> Set.empty
+    | (n, k) ->
         let left = allSubsets(n-1) k
         let right = allSubsets (n - 1) (k - 1)
         let addNToSet subset = Set.add n subset
         Set.union (left) (Set.map addNToSet right)
+        
