106fafd5e86790ea67e718ea5e0400595f272b8f
marche1os <kolpmark@yandex.ru>
2023-06-26 22:35:52 +0300

    add another impl

--- lesson_43/task.fs
+++ lesson_43/task.fs
@@ -5,3 +5,13 @@ let try_find key m =
     match (key, m) with
     | (k, map) when Map.containsKey k map -> Some(Map.find k map)
     | (k, map) -> None
+    
+let rec search (list, key) =
+    match list with
+    | (k, v) :: tail when k = key -> Some(v)
+    | (k, v) :: tail -> search(tail, key)
+    | [] -> None
+
+let try_find2 key m = search (Map.toList m, key)
+
+printfn "%A" (try_find2 1 Map[(1, "Mark"); (2, "Daria")])
